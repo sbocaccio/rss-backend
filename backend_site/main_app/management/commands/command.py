c21f15be408864aad9121998fe55d7e88292dccf
sbocaccio <sbocaccio@devartis.comd>
2021-08-06 18:06:57 -0300

    Add function for crating feeds from OPML. Still have to improve code and separate try/catch better

--- backend_site/main_app/management/commands/command.py
+++ backend_site/main_app/management/commands/command.py
@@ -1,9 +1,46 @@
+import sys
 from django.core.management.base import BaseCommand, CommandError
+from xml.etree import ElementTree
+from ...auxiliary.helpers.feed_helper import SubscriptionFeedHelper
+from django.contrib.auth.models import User
+from rest_framework.serializers import ValidationError
+from termcolor import colored
 
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
+    help = 'Parse OPML file subscription and create subscription then add the users to them.'
 
-
+    def add_arguments(self, parser):
+        parser.add_argument('file', type=str)
+        parser.add_argument('users', nargs='+', type=str)
 
     def handle(self, *args, **options):
-        self.stdout.write('hijh')
+        users = options['users']
+        file = options['file']
+        feeds = self.OPML_parse(file)
+        subscription_helper = SubscriptionFeedHelper()
+        for feed in feeds:
+            for user in users:
+                try :
+                    print(colored('START','green'), 'adding', user,'to subscription', feed)
+                    data = {'link': feed}
+                    usuario = User.objects.get(username = user)
+                    subscription_helper._create_feed(data,usuario)
+                except ValidationError as error:
+                    print(colored('ERROR:','red'),error.detail['message'])
+                except ValueError as error:
+                    print(colored('ERROR:','red'),error)
+                except Exception as error:
+                    print(colored('ERROR:', 'red'), error)
+
+                else:
+                    print(colored('SUCCESS:','green'),'Successfully added user:', user, 'to subscription', feed)
+
+    def OPML_parse(self, file):
+        urls = []
+        with open(file, 'rt') as f:
+            tree = ElementTree.parse(f)
+        for node in tree.findall('.//outline'):
+            url = node.attrib.get('xmlUrl')
+            if url:
+                urls.append(url)
+        return urls
